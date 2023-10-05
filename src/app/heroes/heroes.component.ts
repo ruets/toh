cac7665c49270334c8c53b5f6169d02c6987eb34
ruets <sebastien.ruet@etu.univ-grenoble-alpes.fr>
2023-10-05 08:23:00 +0200

    Tutorial: Step 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Hero } from '../data/hero';
+import { HEROES } from "../data/mock-heroes";
+import {Hero} from "../data/hero";
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +8,10 @@ import { Hero } from '../data/hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
