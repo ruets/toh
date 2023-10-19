26f01ffad44e7c6f8e52cbd676aee17907957324
ruets <sebastien.ruet@etu.univ-grenoble-alpes.fr>
2023-10-19 16:39:20 +0200

    Add firebase support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'tour-of-heroes-65211',
+    appId: '1:68911961878:web:ef68f5c8b32f6a2c602237',
+    storageBucket: 'tour-of-heroes-65211.appspot.com',
+    apiKey: 'AIzaSyCLrI3QB3yx8GM2CnXvAxqeLsBKGWSOmKE',
+    authDomain: 'tour-of-heroes-65211.firebaseapp.com',
+    messagingSenderId: '68911961878',
+    measurementId: 'G-070QZNFHWY',
+  },};
